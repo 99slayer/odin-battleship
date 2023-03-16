fabb3e385a97b35cea53386829d4daa720c7776d
wunderbar <lankyauz@gmail.com>
2023-03-16 14:19:00 -0600

    - Created and tested the parseCellCoordinate function.

---
+++ src/__tests__/parseCellCoordinate.test.js
@@ -0,0 +1,14 @@
+import { parseCellCoordinate } from "../parseCellCoordinate";
+
+describe('testing parseCellCoordinate function',()=>{
+  test('testing 2 number coordinates.',()=>{
+    expect(parseCellCoordinate('1-2')).toBe('A2');
+    expect(parseCellCoordinate('7-7')).toBe('G7');
+  });
+
+  test('testing 2 digit number coordinates.',()=>{
+    expect(parseCellCoordinate('10-3')).toBe('J3');
+    expect(parseCellCoordinate('5-10')).toBe('E10');
+    expect(parseCellCoordinate('10-10')).toBe('J10');
+  });
+})
---
+++ src/parseCellCoordinate.js
@@ -0,0 +1,40 @@
+export const parseCellCoordinate = (attribute) => {
+  if(typeof attribute !== 'string'){
+    return;
+  };
+
+  let arr = attribute.split('');
+
+  const getLetter = (array) => {
+    let letterValue;
+
+    if(!(isNaN(parseInt(array[1])))){
+      let twoDigit = array.slice(0,2);
+      letterValue = twoDigit.join('');
+    } else {
+      letterValue = array[0];
+    };
+
+    let codeValue = parseInt(letterValue);
+    let letter = String.fromCharCode((65 + codeValue) - 1);
+    // console.log(letter);
+    return letter;
+  };
+
+  const getNumber = (array) => {
+    let number;
+    if(!(isNaN(parseInt(array[array.length-2])))){
+      let twoDigit = array.slice(array.length-2);
+      number = twoDigit.join('');
+    } else {
+      number = array[array.length-1];
+    };
+    // console.log(number);
+    return number;
+  };
+
+  let letter = getLetter(arr);
+  let number = getNumber(arr);
+  // console.log(letter + number);
+  return letter + number;
+}
